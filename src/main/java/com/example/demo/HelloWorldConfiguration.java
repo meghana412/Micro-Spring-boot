66a6f6c9310e39187efa7301a3ddf142ed32840a
Meghana <meghanakagitha@gmail.com>
2017-08-16 14:48:52 -0500

    initial commit

---
+++ src/main/java/com/example/demo/HelloWorldConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.dropwizard.Configuration;
+
+
+public class HelloWorldConfiguration extends Configuration{
+	
+	@NotEmpty
+    @JsonProperty
+    private String template;
+    @NotEmpty
+    @JsonProperty
+    private String defaultName = "Stranger";
+    public String getTemplate() {
+        return template;
+    }
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+}
---
+++ src/main/java/com/example/demo/MicroSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroSpringbootApplication.class, args);
+	}
+}
